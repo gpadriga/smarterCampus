d6efad4a109277bc4ab068c4c73795e1038c0f2c
Gabby <gpadriga@stevens.edu>
2018-06-12 13:44:27 -0400

    here we go!

---
+++ sensorReading.py
@@ -0,0 +1,44 @@
+# Read data from all sensors and print
+# Includes: BME680, TSL2561 Light Sensor
+
+import bme680
+import time
+import os
+
+# Some variables
+REPEAT = 5
+WAIT_PERIOD = 2
+
+def main():
+    count = 0
+    bme = bme680.BME680(i2c_addr=0x77)
+
+    #Initialize sensor
+    bme.set_humidity_oversample(bme680.OS_2X)
+    bme.set_pressure_oversample(bme680.OS_4X)
+    bme.set_temperature_oversample(bme680.OS_8X)
+    bme.set_filter(bme680.FILTER_SIZE_3)
+    bme.set_gas_status(bme680.ENABLE_GAS_MEAS)
+
+    # Main loop
+    while (count < REPEAT):
+		if bme.get_sensor_data():
+			temperatureCelcius = float("{0:.2f}".format(sensor.data.temperature))
+			#Convert the above variable to fahrenheit
+			temperature = float(temperatureCelcius*(9/5) + 32)
+			pressure = float("{0:.2f}".format(sensor.data.pressure))
+			humidity = float("{0:.2f}".format(sensor.data.humidity))
+			gas = float("{0:.2f}".format(sensor.data.gas_resistance))
+
+			print("Temperature: " + str(temperature))
+			print("Pressure:" + str(pressure))
+			print("Humidity:" + str(humidity))
+			print("Gas:" + str(gas) + '\n')
+			
+			count += 1
+			
+			time.sleep(WAIT_PERIOD)
+
+# Run main
+if __name__ == '__main__':
+    main()
