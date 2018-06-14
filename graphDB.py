74a0f024bab99cb21d9bafea4f03971d65644e3d
Gabby Padriga <emailgabby@gmail.com>
2018-06-14 14:03:08 -0500

    Graphs temp and lux via plotly

---
+++ graphDB.py
@@ -0,0 +1,38 @@
+import sqlite3
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly import tools
+
+con = sqlite3.connect("data.db")
+c = con.cursor()
+
+c.execute('SELECT temp, lux FROM data')
+elems = c.fetchall()
+
+temps=[]
+luxs=[]
+counts = []
+count = 0
+
+for row in elems:
+	temps += [row[0]]
+	luxs += [row[1]]
+	count += 1
+	counts += [count]
+
+con.commit()
+con.close()
+
+TemperatureF = go.Scatter(
+	x=counts,
+	y=temps
+)
+
+Lux = go.Scatter(
+	x=counts,
+	y=luxs
+)
+
+graphs = [TemperatureF, Lux]
+
+py.iplot(graphs, filename='testing')
