7490ec292d646baf9739d42018c425717e7a7d83
Gabby Padriga <emailgabby@gmail.com>
2018-06-14 12:06:17 -0500

    added luxVal

--- sensorReading.py
+++ sensorReading.py
@@ -7,15 +7,14 @@ import time
 import os
 
 # Some variables
-REPEAT = 5
 WAIT_PERIOD = 2
 
 def main():
-    count = 0
     bme = bme680.BME680(i2c_addr=0x77)
 
     #Initialize sensor
-    bme.set_humidity_oversample(bme680.OS_2X)
+    bme.set_humidity_oversample(b
+                                me680.OS_2X)
     bme.set_pressure_oversample(bme680.OS_4X)
     bme.set_temperature_oversample(bme680.OS_8X)
     bme.set_filter(bme680.FILTER_SIZE_3)
@@ -23,31 +22,29 @@ def main():
 
     # Main loop
     while (1):
-		if bme.get_sensor_data():
-			tempCelcius = float("{0:.2f}".format(bme.data.temperature))
-			#Convert the above variable to fahrenheit
-			temperature = float(tempCelcius*(9/5) + 32)
-			pressure = float("{0:.2f}".format(bme.data.pressure))
-			humidity = float("{0:.2f}".format(bme.data.humidity))
-			gas = float("{0:.2f}".format(bme.data.gas_resistance))
+        bme.get_sensor_data()
+        tempCelcius = float("{0:.2f}".format(bme.data.temperature))
+	#Convert the above variable to fahrenheit
+        temperature = float(tempCelcius*(9/5) + 32)
+        pressure = float("{0:.2f}".format(bme.data.pressure))
+        humidity = float("{0:.2f}".format(bme.data.humidity))
+        gas = float("{0:.2f}".format(bme.data.gas_resistance))
 			
-			# Read from lux sensor
-			tsl = TSL2561(debug=True)
+        # Read from lux sensor
+        tsl = TSL2561(debug=True)
+        luxVal = tsl.lux()
+        
+        print("      BME680")
+        print("Temperature: {}".format(temperature))
+        print("Pressure: {}".format(pressure))
+        print("Humidity: {}".format(humidity))
+        print("Gas: {}".format(gas))
+        print('\n')
+        print("     TSL2561")
+        print("Lux: {}".format(luxVal))
+        print ("------------------------")
 			
-			print("      BME680")
-			print("Temperature: {}".format(temperature))
-			print("Pressure: {}".format(pressure))
-			print("Humidity: {}".format(humidity))
-			print("Gas: {}".format(gas))
-			print('\n')
-			
-			print("     TSL2561")
-			print("Lux: {}".format(tsl.lux()))
-			print ("------------------------")
-			
-			count += 1
-			
-			time.sleep(WAIT_PERIOD)
+        time.sleep(WAIT_PERIOD)
 
 # Run main
 if __name__ == '__main__':
