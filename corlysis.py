1cd71f5dc0847332e15bd901524a86d96302cb7d
SHAPS252 <dshap252@gmail.com>
2018-07-09 12:03:22 -0400

    local db reading now in corlysis.py fancy reading data line now included

--- corlysis.py
+++ corlysis.py
@@ -1,4 +1,5 @@
 # Read data from all sensors and print
+# also send to local DB
 # Includes: BME680, TSL2561 Light Sensor, USB microphone test
 # Remember to run like "python corlysis.py data [token]"
 
@@ -12,6 +13,7 @@ import pyaudio
 import analyse
 import requests
 import argparse
+import sys
 
 # Some variables
 REPEAT = 5
@@ -23,6 +25,10 @@ MAX_LINES_HISTORY = 1000
 
 def main():
     bme = bme680.BME680(i2c_addr=0x77)
+    #Initialize local db
+    con = sqlite3.connect('corlysisData.db')
+    c = con.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS data(temp FLOAT, pres FLOAT, hum FLOAT, gas FLOAT, lux INTEGER, db FLOAT, dt DATETIME)''')
 
     # Initialize db
     parser = argparse.ArgumentParser()
@@ -52,73 +58,99 @@ def main():
     counter = 1
     problem_counter = 0
 
+    now = time.strftime('%Y-%m-%d %H:%M:%S')
+    print("Readings began " + now)
+    print("Press ctrl+c to end readings and close connection.")
+
+    animation = "|/-\\"
+    aniCount = 0
+	
     # Main loop
     while (True):
-	    unix_time_ms = int(time.time()*1000)
-		
-	    # Read from BME
-	    bme.get_sensor_data()
-	    tempCelcius = float("{0:.2f}".format(bme.data.temperature))
-	    #Convert the above variable to fahrenheit
-	    temperature = float(tempCelcius*(9/5) + 32)
-	    pressure = float("{0:.2f}".format(bme.data.pressure))
-	    humidity = float("{0:.2f}".format(bme.data.humidity))
-	    gas = float("{0:.2f}".format(bme.data.gas_resistance))
+		try: 
+			# Get time for corlysis and db
+			unix_time_ms = int(time.time()*1000)
+			now = time.strftime('%Y-%m-%d %H:%M:%S')
 			
-	    # Read from lux sensor
-	    tsl = TSL2561(debug=True)
-	    luxVal = tsl.lux()
-	    
-	    # Read from USB mic
-            rawsamps = stream.read(2048, exception_on_overflow=False)
-            samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
-            dB = analyse.loudness(samps) + 60
-	
-	    line = "sensors_data temperature={},pressure={},humidity={},luxVal={},decib={} {}\n".format(temperature,
-             pressure,
-             humidity,
-             luxVal,
-             dB,
-             unix_time_ms)
-            payload += line
-		
-	    if counter % SENDING_PERIOD == 0:
-            	try:
-                	# try to send data to cloud
-                	r = requests.post(URL, params=corlysis_params, data=payload)
-                	if r.status_code != 204:
-                    		raise Exception("data not written")
-                	payload = ""
-            	except:
-                	problem_counter += 1
-                	print('cannot write to InfluxDB')
-                	if problem_counter == MAX_LINES_HISTORY:
-                    		problem_counter = 0
-                    		payload = ""
-
-            counter += 1
-		
-	    print("      BME680")
-	    print("Temperature: {}".format(temperature))
-	    print("Pressure: {}".format(pressure))
-	    print("Humidity: {}".format(humidity))
-	    print("Gas: {}".format(gas))
-	    print('\n')
-	    print("     TSL2561")
-            print("Lux: {}".format(luxVal))
-            print('\n')
-            print("     USB Mic")
-            print ("------------------------")
-            print ("Sound in dB: {}".format(dB)) 
-            
+			# Read from BME
+			bme.get_sensor_data()
+			tempCelcius = float("{0:.2f}".format(bme.data.temperature))
+			#Convert the above variable to fahrenheit
+			temperature = float(tempCelcius*(9/5) + 32)
+			pressure = float("{0:.2f}".format(bme.data.pressure))
+			humidity = float("{0:.2f}".format(bme.data.humidity))
+			gas = float("{0:.2f}".format(bme.data.gas_resistance))
+				
+			# Read from lux sensor
+			tsl = TSL2561(debug=True)
+			luxVal = tsl.lux()
 			
-	    time_diff_ms = int(time.time()*1000) - unix_time_ms
-            print(time_diff_ms)
-            if time_diff_ms < READING_DATA_PERIOD_MS:
-            	time.sleep((READING_DATA_PERIOD_MS - time_diff_ms)/1000.0)
+			# Read from USB mic
+			rawsamps = stream.read(2048, exception_on_overflow=False)
+			samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+			dB = analyse.loudness(samps) + 60
+		
+			line = "sensors_data temperature={},pressure={},humidity={},luxVal={},decib={} {}\n".format(temperature,
+				 pressure,
+				 humidity,
+				 luxVal,
+				 dB,
+				 unix_time_ms)
+			payload += line
+			
+			if counter % SENDING_PERIOD == 0:
+					try:
+						# try to send data to cloud
+						r = requests.post(URL, params=corlysis_params, data=payload)
+						if r.status_code != 204:
+								raise Exception("data not written")
+						payload = ""
+					except:
+						problem_counter += 1
+						print('cannot write to InfluxDB')
+						if problem_counter == MAX_LINES_HISTORY:
+								problem_counter = 0
+								payload = ""
 
-    con.commit()
-    con.close()
+			counter += 1
+			
+			#print("      BME680")
+			#print("Temperature: {}".format(temperature))
+			#print("Pressure: {}".format(pressure))
+			#print("Humidity: {}".format(humidity))
+			#print("Gas: {}".format(gas))
+			#print('\n')
+			#print("     TSL2561")
+			#print("Lux: {}".format(luxVal))
+			#print('\n')
+			#print("     USB Mic")
+			#print ("------------------------")
+			#print ("Sound in dB: {}".format(dB)) 
+			
+			sys.stdout.write("\rCollecting data... " + animation[aniCount])
+			sys.stdout.flush()
+			aniCount += 1
+			if (aniCount == 4):
+				aniCount = 0
+				
+			time_diff_ms = int(time.time()*1000) - unix_time_ms
+			#print(time_diff_ms)
+			if time_diff_ms < READING_DATA_PERIOD_MS:
+				time.sleep((READING_DATA_PERIOD_MS - time_diff_ms)/1000.0)
+				
+			values = (temperature, pressure, humidity, gas, luxVal, dB, now)
+			c.execute("INSERT INTO data VALUES(?, ?, ?, ?, ?, ?, ?)", values)
+
+			con.commit()
+				
+		except KeyboardInterrupt:
+			con.close()
+			break
+		
+		except Exception as e:
+			pass
+			print(e)
+
 # Run main
 if __name__ == '__main__':
     main()
